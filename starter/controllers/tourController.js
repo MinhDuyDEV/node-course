30c4c2a0d06e2bf1b0959b0805715fb5e5aa8121
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-03-17 21:33:34 +0700

    making api limiting field

--- starter/controllers/tourController.js
+++ starter/controllers/tourController.js
@@ -17,6 +17,13 @@ exports.getAllTour = async (req, res) => {
     } else {
       query = query.sort('createdAt');
     }
+    // Todo: Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
     // * EXECUTE QUERY
     const tours = await query;
     // * SEND RESPONSE
