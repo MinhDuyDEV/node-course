923ab15a8e8e612c9d1ac7c670fc348fdfd2f202
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-03-17 14:40:18 +0700

    making api (filtering)

--- starter/controllers/tourController.js
+++ starter/controllers/tourController.js
@@ -6,7 +6,14 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTour = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    const query = Tour.find(req.query);
+    // EXECUTE QUERY
+    const tours = await query;
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
