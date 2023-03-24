bd800684116f0ce8ba943f47e80239a5567c463e
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-03-24 19:30:28 +0700

    protecting tour routes

--- starter/controllers/authController.js
+++ starter/controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
@@ -14,6 +15,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt,
   });
   const token = signToken(newUser._id);
   res.status(201).json({
@@ -42,4 +44,41 @@ exports.login = catchAsync(async (req, res, next) => {
     status: 'success',
     token,
   });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  // Todo1: Getting token and check of it's there
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access', 401)
+    );
+  }
+  // Todo2: Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  // Todo3: Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError(
+        'The user belonging to this token does no longer exists',
+        401
+      )
+    );
+  }
+  // Todo4: Check if user change password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again.', 401)
+    );
+  }
+  // TODO GRANT ACCESS TO PROTECT ROUTE
+  req.user = currentUser;
+  next();
 });
