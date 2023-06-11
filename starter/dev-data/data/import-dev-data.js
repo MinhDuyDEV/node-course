53d43ee55b1baaa1c2a782ee293a1c7fd2dd365b
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-06-11 22:10:43 +0700

    calc average rating on tours

--- starter/dev-data/data/import-dev-data.js
+++ starter/dev-data/data/import-dev-data.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 mongoose.set('strictQuery', false);
 dotenv.config({ path: 'config.env' });
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+const DB =
+  'mongodb+srv://minhduy:Ule9n4Wv2uwMz8F7@cluster0.peeo4v1.mongodb.net/natours?retryWrites=true&w=majority';
 async function dbConnect() {
   await mongoose
     .connect(DB)
@@ -17,11 +17,17 @@ async function dbConnect() {
 
 // READ JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 // IMPORT DATA INTO DATABASE
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully loaded!');
   } catch (error) {
     console.log(error);
@@ -33,6 +39,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data successfully deleted!');
   } catch (error) {
     console.log(error);
